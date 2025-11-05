c447ec4bbaeef37fe42ee6fa4de277279b254514
Romulo Rosa | FullStack | React + TS <romulorosafullstack@gmail.com>
2025-11-05 17:21:13 -0300

    component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import "./index.scss"
+import Nav from "./Nav"
+import Button from "../Button"
+
+export default function Header() {
+  return (
+    <header className="header__wrapper">
+      <div className="header__content">
+        <h1 className="header__logo">ReciclaTech</h1>
+        <div className="header__navbutton">
+          <Nav />
+          <Button
+            cta="Começar Agora"
+            className="button-1"
+            url="https://github.com/romulorosafullstack/2025-vainaweb-reciclatech"
+          />
+        </div>
+      </div>
+    </header>
+  )
+}

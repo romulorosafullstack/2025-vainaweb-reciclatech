f3b664888bf549a0260f7d99352479b237a84785
Romulo Rosa | FullStack | React + TS <romulorosafullstack@gmail.com>
2025-11-07 14:48:23 -0300

    Header conclusao desktop

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="header__wrapper">
       <div className="header__content">
-        <h1 className="header__logo">ReciclaTech</h1>
+        <h1 className="header__logo" title="ReciclaTech">ReciclaTech</h1>
         <div className="header__navbutton">
           <Nav />
           <Button
